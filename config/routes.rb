dbe9145ea90405e7635134067e6814ebb03df62f
Eric Hsu <erichsu7@gmail.com>
2018-06-10 19:12:24 -0700

    Create StepsController.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
+    resources :steps, only: [:index, :create, :update, :destroy]
     resources :todos, only: [:show, :index, :create, :update, :destroy]
   end
 end
